c101d86db13e81947561cc6eadca8565cff36637
elaine <e.cristina00@gmail.com>
2022-03-04 23:36:32 -0300

    Começando novoi módulo

---
+++ src/herdado/Account.java
@@ -0,0 +1,90 @@
+package herdado;
+
+import com.bytebank.Conta;
+
+public class Account {
+    private int agencia;
+    private int numero;
+    private double saldo;
+    private Client titular;
+    private static int total; // atributo da Classe,
+
+    public Account(int agencia, int numero){
+        Account.total ++;
+        System.out.println("O total de contas abertas até o momento é: " + total);
+        this.agencia = agencia;
+        this.numero = numero;
+    }
+
+    public int getAgencia(){
+
+        return agencia;
+    }
+    public void setAgencia(int agencia){
+        if(agencia <=0){
+            System.out.println("A agência não pode ter numeros iguais ou inferior a zero!");
+            return;
+        }
+        this.agencia = agencia;
+    }
+    public int getNumero(){
+
+        return numero;
+    }
+    public void setNumero(int numero){
+        if (numero <=0){
+            System.out.println("O numero da conta não pode ter numeros iguais ou inferior a zero!");
+        }
+        this.numero = numero;
+    }
+    public double getSaldo() {
+
+        return saldo;
+    }
+    /*public void setSaldo (double saldo){
+        this.saldo = saldo;
+    }*/
+
+    public Client getTitular() {
+
+        return titular;
+    }
+
+    public void setTitular(Client titular) {
+
+        this.titular = titular;
+    }
+
+
+    //o static permite que as instancias acessem os dados da classe.
+    public static int getTotal(){
+        return Account.total;
+    }
+
+    public void deposita(double valor){
+
+        this.saldo = this.saldo + valor;
+    }
+
+
+
+    public boolean sacar(double valor){
+        if (this.saldo >= valor){
+            this.saldo = this.saldo - valor;
+            return true;
+        }else{
+            return false;
+        }
+
+    }
+
+    public boolean transferir(double valor, Conta destino){
+        if (this.saldo >= valor){
+            this.sacar(valor);
+            destino.deposita(valor);
+            return true;
+        }else {
+            return false;
+        }
+    }
+}
---
+++ src/herdado/Client.java
@@ -0,0 +1,29 @@
+package herdado;
+
+public class Client {
+    private String name;
+    private String cpf;
+    private String work;
+
+    public String getName(){
+        return name;
+    }
+    public void setName (String name){
+        this.name = name;
+    }
+
+    public String getCpf(){
+        return cpf;
+    }
+
+    public void setCpf(String cpf){
+        this.cpf = cpf;
+    }
+
+    public String getWork(){
+        return work;
+    }
+    public void setWork(String work){
+        this.work = work;
+    }
+}
