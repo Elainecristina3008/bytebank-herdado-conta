21b0e64dee83e8d6a5dbc9506a1032ce214ebe7d
Elaine <e.cristina00@gmail.com>
2022-06-01 22:06:02 -0300

    Usando o finally

---
+++ src/herdado/TesteConexao.java
@@ -0,0 +1,18 @@
+package herdado;
+
+public class TesteConexao {
+    public static void main(String[] args) {
+        Conexao con = null;
+        try {
+            con = new Conexao();
+            con.leDados();
+            //con.fecha();
+        }catch (IllegalStateException ex){
+            System.out.println("Deu erro na conexão");
+            //con.fecha();
+        }finally {
+            con.fecha();
+        }
+    }
+
+}
